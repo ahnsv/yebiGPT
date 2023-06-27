57f6d0fd8cb78f011d10209941ea3ade9a56aca9
ahnsv <ahnsv@bc.edu>
2023-06-27 17:09:01 +0900

    feat: project init

---
+++ yebigpt/main.py
@@ -0,0 +1,40 @@
+import os 
+from langchain.document_loaders import PyMuPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import RetrievalQA
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+persist_directory = "./storage"
+pdf_path = "./data/yebigun_2023_qna.pdf"
+
+loader = PyMuPDFLoader(pdf_path)
+documents = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=512, chunk_overlap=10)
+texts = text_splitter.split_documents(documents)
+
+embeddings = OpenAIEmbeddings()
+vectordb = Chroma.from_documents(documents=texts, 
+                                 embedding=embeddings,
+                                 persist_directory=persist_directory)
+vectordb.persist()
+
+retriever = vectordb.as_retriever(search_kwargs={"k": 3})
+llm = ChatOpenAI(model_name='gpt-3.5-turbo-0613')
+
+qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
+
+while True:
+        user_input = input("Enter a query: ")
+        if user_input == "exit":
+            break
+
+        query = f"###Prompt {user_input}"
+        try:
+            llm_response = qa(query)
+            print(llm_response["result"])
+        except Exception as err:
+            print('Exception occurred. Please try again', str(err))
